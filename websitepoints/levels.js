0f665476f5a667d7365aefee27fa95a9c9353848
LeeSquash <holdway@hotmail.com>
2026-07-23 15:44:47 +0100

    Add files via upload

--- websitepoints/levels.js
+++ websitepoints/levels.js
@@ -5,26 +5,17 @@
 
 import { POINTS_CONFIGURATION } from "./configuration.js";
 
-
 export function calculateLevel(totalPoints) {
 
     let level = POINTS_CONFIGURATION.levels.startingLevel;
-
     let pointsNeeded = POINTS_CONFIGURATION.levels.firstLevelUpPoints;
-
-    let increase = POINTS_CONFIGURATION.levels.pointsIncreasePerLevel;
-
+    let increase = POINTS_CONFIGURATION.levels.firstLevelUpPoints;
 
     while (totalPoints >= pointsNeeded) {
-
         level++;
-
-        pointsNeeded += increase;
-
         increase += POINTS_CONFIGURATION.levels.pointsIncreasePerLevel;
-
+        pointsNeeded += increase;
     }
-
 
     return level;
 
